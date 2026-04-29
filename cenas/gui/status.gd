9572145b7e06b1de8a22ee0c5c54e7a00c168d4f
Renan UFPB <renantomazini@sti.ufpb.br>
2026-04-29 16:44:49 -0300

    adicionando

--- cenas/gui/status.gd
+++ cenas/gui/status.gd
@@ -1,4 +1,5 @@
 extends Node2D
+const ItensDB = preload("res://scripts/ItensDB.gd")
 var nome = ''
 var curso_nome = ''
 var fomev = 33.00
@@ -11,7 +12,7 @@ var level_prof = 1
 var curso = 0
 var dia = 0
 var minutos = 720
-
+@onready var itens = ItensDB.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -32,7 +33,7 @@ func add_fome(valor):
 	
 func add_estresse(valor):
 	estressev = estressev + valor
-	$fome.set_perc(estressev)
+	$estresse.set_perc(estressev)
 	
 func add_energia(valor):
 	energiav = energiav + valor
@@ -40,11 +41,11 @@ func add_energia(valor):
 	
 func add_social(valor):
 	socialv = socialv + valor
-	$fome.set_perc(socialv)
+	$social.set_perc(socialv)
 	
 func add_sede(valor):
 	sedev = sedev + valor
-	$fome.set_perc(sedev)
+	$sede.set_perc(sedev)
 	
 	
 func set_nome(nomestr):
@@ -98,3 +99,49 @@ func _on_botoes_comer_menu() -> void:
 
 func _on_button_3_pressed() -> void:
 	$Money.set_din(3100.21)
+
+
+
+
+func carregar_lista(lista):
+	$ItemList.clear()
+	
+	for item in lista:
+		var index = $ItemList.add_item(
+			item["nome"] + "\nR$" + str(item["preco"]),
+			item["icone"]
+		)
+		
+		$ItemList.set_item_metadata(index, item)
+
+var menu_aberto = ""
+
+func _on_button_comer_pressed():
+	if menu_aberto == "comida":
+		$ItemList.clear()
+		$ItemList.set_visible(false)
+		menu_aberto = ""
+	else:
+		carregar_lista(itens.comidas)
+		menu_aberto = "comida"
+		$ItemList.set_visible(true)
+
+
+func _on_button_beber_pressed():
+	if menu_aberto == "bebida":
+		$ItemList.clear()
+		$ItemList.set_visible(false)
+		menu_aberto = ""
+	else:
+		carregar_lista(itens.bebidas)
+		$ItemList.set_visible(true)
+		menu_aberto = "bebida"
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	var item = $ItemList.get_item_metadata(index)
+	print(item)
+	$ItemList.clear()
+	$ItemList.set_visible(false)
+	menu_aberto = ""
+	
---
+++ scripts/ItensDB.gd
@@ -0,0 +1,93 @@
+
+@export var comidas = [
+	{
+		"nome": "Hamburguer",
+		"icone": preload("res://icon.svg"),
+		"preco": 25,
+		"energia": 10,
+		"estresse": -5,
+		"sono": -2
+	},
+	{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+		{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+		{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+		{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+		{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+			{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+			{
+		"nome": "Café",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	},
+				{
+		"nome": "coxinha",
+		"icone": preload("res://icon.svg"),
+		"preco": 10,
+		"energia": 20,
+		"estresse": -2,
+		"sono": -10
+	}
+]
+
+@export var bebidas = [
+		{
+		"nome": "Chá",
+		"icone": preload("res://icon.svg"),
+		"preco": 0.25,
+		"energia": 10,
+		"estresse": -5,
+		"sono": -2
+	},	{
+		"nome": "Refri",
+		"icone": preload("res://icon.svg"),
+		"preco": 1.25,
+		"energia": 10,
+		"estresse": -5,
+		"sono": -2
+	}
+]
